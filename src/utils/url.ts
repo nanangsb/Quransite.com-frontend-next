035aacc33306470c76e5974ea2f5870f04236df9
nanangsb <sefida.nanang@gmail.com>
2024-05-16 08:14:18 +0700

    Update url.ts

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,4 +1,4 @@
-const getLocalePostfix = (locale: string) => (locale !== 'id' ? `/${locale}` : '');
+const getLocalePostfix = (locale: string) => (locale !== 'en' ? `/${locale}` : '');
 
 export const getCurrentPath = () => {
   if (typeof window !== 'undefined') {
