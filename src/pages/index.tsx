e9da07e5088fa30b25a83ef1611d2a613c9eb2fe
nanangsb <sefida.nanang@gmail.com>
2024-05-23 15:45:42 +0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,13 +40,15 @@ const Index: NextPage<IndexProps> = ({ chaptersResponse: { chapters } }): JSX.El
       <div className={styles.pageContainer}>
         <div className={styles.flow}>
           <HomePageHero />
+          
           <div className={classNames(styles.flowItem, styles.fullWidth)}>
             <BookmarksAndCollectionsSection isHomepage />
           </div>
           <div className={styles.flowItem}>
             <ChapterAndJuzListWrapper chapters={chapters} />
           </div>
-          <div className={classNames(styles.flowItem, styles.fullWidth)}> 
+
+          <div className={classNames(styles.flowItem, styles.fullWidth)}>
             <RamadanActivitiesSection />
           </div>
           <div className={classNames(styles.flowItem, styles.fullWidth)}>
