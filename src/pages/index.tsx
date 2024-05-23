ca8ef8effe4dce1c9c99d221dcb4d42f6d9fc481
nanangsb <sefida.nanang@gmail.com>
2024-05-23 16:10:18 +0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,8 @@ import styles from './index.module.scss';
 
 import ChapterAndJuzListWrapper from '@/components/chapters/ChapterAndJuzList';
 import HomePageHero from '@/components/HomePage/HomePageHero';
+import QuranGrowthJourneySection from '@/components/HomePage/QuranGrowthJourneySection';
+import RamadanActivitiesSection from '@/components/HomePage/RamadanActivitiesSection';
 import NextSeoWrapper from '@/components/NextSeoWrapper';
 import BookmarksAndCollectionsSection from '@/components/Verses/BookmarksAndCollectionsSection';
 import { getAllChaptersData } from '@/utils/chapter';
@@ -38,15 +40,18 @@ const Index: NextPage<IndexProps> = ({ chaptersResponse: { chapters } }): JSX.El
       <div className={styles.pageContainer}>
         <div className={styles.flow}>
           <HomePageHero />
-          
+          <div className={classNames(styles.flowItem, styles.fullWidth)}>
+            <RamadanActivitiesSection />
+          </div>
+          <div className={classNames(styles.flowItem, styles.fullWidth)}>
+            <QuranGrowthJourneySection />
+          </div>
           <div className={classNames(styles.flowItem, styles.fullWidth)}>
             <BookmarksAndCollectionsSection isHomepage />
           </div>
           <div className={styles.flowItem}>
             <ChapterAndJuzListWrapper chapters={chapters} />
           </div>
-
-          
         </div>
       </div>
     </>
